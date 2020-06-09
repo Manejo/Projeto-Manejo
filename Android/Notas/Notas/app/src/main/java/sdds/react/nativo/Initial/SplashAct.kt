9a172e0de18a249f04af908f9977e9c593687f58
Alcantara9 <lucasalcantara999@gmail.com>
2020-06-09 19:03:49 -0300

    Finalizando ajustes finais do projeto: Adição de icone e nova rota do mapa.

--- Android/Notas/Notas/app/src/main/java/sdds/react/nativo/Initial/SplashAct.kt
+++ Android/Notas/Notas/app/src/main/java/sdds/react/nativo/Initial/SplashAct.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import sdds.react.nativo.R
+import sdds.react.nativo.Trilha.TrilhaList
 
 class SplashAct : AppCompatActivity() {
 
